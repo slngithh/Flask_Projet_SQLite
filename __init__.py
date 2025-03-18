229cb19f652c69ac5b40f994772ee138bab32d67
slngithh <solene.bernabe@edu.esiee-it.fr>
2025-03-18 14:35:19 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from flask import json
 from urllib.request import urlopen
 from werkzeug.utils import secure_filename
 import sqlite3
-#
+
 app = Flask(__name__)                                                                                                                  
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'  # Clé secrète pour les sessions
 
